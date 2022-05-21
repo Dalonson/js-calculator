56ab7a7a2c546fb9236c5e3b73420db15b01bd60
D.Vin <94095647+Dalonson@users.noreply.github.com>
2022-05-21 14:24:44 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const initApp = () => {
+  const currentValueElem = document.querySelector(".currentvalue");
+  const previousValueElem = document.querySelector(".previousvalue");
+  let itemArray = [];
+  const equationArray = [];
+  let newNumberFlag = false;
+
+  const inputButtons = document.querySelectorAll(".number");
+  inputButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const newInput = event.target.textContent;
+      if (newNumberFlag) {
+        currentValueElem.value = newInput;
+        newNumberFlag = false;
+      } else {
+        currentValueElem.value =
+          currentValueElem.value == 0
+            ? newInput
+            : `${currentValueElem.value}${newInput}`;
+      }
+    });
+  });
+
+  const clearButtons = document.querySelectorAll(".clear, .clear-entry");
+  clearButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      currentValueElem.value = 0;
+      if (event.target.classList.contains("clear")) {
+        previousValueElem.textContent = "";
+        itemArray = [];
+      }
+    });
+  });
+
+  const deleteButton = document.querySelector(".delete");
+  deleteButton.addEventListener("click", () => {
+    currentValueElem.value = currentValueElem.value.slice(0, -1);
+  });
+
+  const signChangeButton = document.querySelector(".sign-change");
+  signChangeButton.addEventListener("click", () => {
+    currentValueElem.value = parseFloat(currentValueElem.value) * -1;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
